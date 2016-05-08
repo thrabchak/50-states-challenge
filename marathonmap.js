43b81d7beb51013476c3ecc9f64444bce5c39e2e
Tom Hrabchak <thrabchak@gmail.com>
2016-05-08 17:14:10 -0400

    Added structure for transforming sheets data to chart data

--- marathonmap.js
+++ marathonmap.js
@@ -1,13 +1,88 @@
-google.charts.load('current', {
-  'packages': ['geochart']
-});
-google.charts.setOnLoadCallback(drawRegionsMap);
+/**
+ * @file 
+ * Reads marathon data from a public google spreadsheet and transforms
+ * it into a geochart.
+ *
+ */
+
+/* Globals */
+var chartData = [['State', 'Completed']];
+var us_states = {
+  'AL': 'Alabama',
+  'AK': 'Alaska',
+  'AZ': 'Arizona',
+  'AR': 'Arkansas',
+  'CA': 'California',
+  'CO': 'Colorado',
+  'CT': 'Connecticut',
+  'DE': 'Delaware',
+  'FL': 'Florida',
+  'GA': 'Georgia',
+  'HI': 'Hawaii',
+  'ID': 'Idaho',
+  'IL': 'Illinois',
+  'IN': 'Indiana',
+  'IA': 'Iowa',
+  'KS': 'Kansas',
+  'KY': 'Kentucky',
+  'LA': 'Louisiana',
+  'ME': 'Maine',
+  'MD': 'Maryland',
+  'MA': 'Massachusetts',
+  'MI': 'Michigan',
+  'MN': 'Minnesota',
+  'MS': 'Mississippi',
+  'MO': 'Missouri',
+  'MT': 'Montana',
+  'NE': 'Nebraska',
+  'NV': 'Nevada',
+  'NH': 'New Hampshire',
+  'NJ': 'New Jersey',
+  'NM': 'New Mexico',
+  'NY': 'New York',
+  'NC': 'North Carolina',
+  'ND': 'North Dakota',
+  'OH': 'Ohio',
+  'OK': 'Oklahoma',
+  'OR': 'Oregon',
+  'PA': 'Pennsylvania',
+  'RI': 'Rhode Island',
+  'SC': 'South Carolina',
+  'SD': 'South Dakota',
+  'TN': 'Tennessee',
+  'TX': 'Texas',
+  'UT': 'Utah',
+  'VT': 'Vermont',
+  'VA': 'Virginia',
+  'WA': 'Washington',
+  'WV': 'West Virginia',
+  'WI': 'Wisconsin',
+  'WY': 'Wyoming'
+}
+var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1HQEdDuUNbra9mGGUl390EaJlZ5wktay3XixThP09fQ8/pubhtml';
+var runner_name = "Sue Hrabchak";
+var stateData = {};
+
+/* Retrieve data from Google Sheets */
+
+function parseStateData(race_log, races) {
+  for (var i = 0; i < us_states.length; i++) {
+  }
+}
+
+function transformToChartData() {
+}
+
+function populateData(data) {
+  parseStateData(data.race_log, data.races);
+  transformToChartData();
+}
+
+/* GeoChart Setup */
 
 function drawRegionsMap() {
 
-  var data = google.visualization.arrayToDataTable([
-    ['State', 'Value'],
-  ]);
+  var data = google.visualization.arrayToDataTable(chartData);
 
   var options = {
     region: 'US',
@@ -17,3 +92,27 @@ function drawRegionsMap() {
   var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
   chart.draw(data, options);
 }
+
+/* main */
+function initStateData() {
+
+  for (var i = 0; i < us_states.length; i++) {
+    var state_abbr = us_states[i].key();
+    var new_obj = {state_abbr: []};
+    stateData.push(new_obj);
+  }
+
+}
+
+
+function main() {
+  initStateData();
+  Tabletop.init( { key: public_spreadsheet_url,
+                          callback: populateData,
+                          simpleSheet: false } );
+
+  google.charts.load('current', {'packages': ['geochart']});
+  google.charts.setOnLoadCallback(drawRegionsMap);
+}
+
+main();

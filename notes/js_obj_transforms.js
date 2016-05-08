812885fca4ea74651dcecc443a25bd82dff7d49b
Tom Hrabchak <thrabchak@gmail.com>
2016-05-08 17:13:33 -0400

    Added object transform notes

---
+++ notes/js_obj_transforms.js
@@ -0,0 +1,45 @@
+data = {
+  'AL': [
+      {
+          // Race
+          date: '2/4/24',
+          name: 'asf',
+          city: 'safd',
+          raceid: 'asdf',
+          
+      },
+      {}
+  ],
+  'AK': 'Alaska',
+  'AZ': 'Arizona',
+  'AR': 'Arkansas',
+  'CA': 'California',
+  'CO': 'Colorado',
+  'CT': 'Connecticut',
+  'DE': 'Delaware',
+  'FL': 'Florida',
+  'GA': 'Georgia',
+  'HI': 'Hawaii',
+  'ID': 'Idaho',
+  'IL': 'Illinois',
+  'IN': 'Indiana',
+  'IA': 'Iowa',
+}
+
+[
+    ['State', 'Completed', 'Tooltip'],
+    ['AK', 0],
+    ['AZ', 0],
+    ['AR', 0],
+    ['CA', 0],
+    ['CO', 0],
+    ['CT', 0],
+    ['DE', 0],
+    ['FL', 0],
+    ['GA', 0],
+    ['HI', 0],
+    ['ID', 0],
+    ['IL', 0],
+    ['IN', 0],
+    ['IA', 0],
+]
